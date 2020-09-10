3e2ab096a5800a8c9c8d05d66dc682540660dbf4
DSwutang <62921599+DSwutang@users.noreply.github.com>
2020-09-10 21:39:43 +0800

    fin epiData

--- app/src/main/java/com/java/linzexi/EpiDataDayModel.java
+++ app/src/main/java/com/java/linzexi/EpiDataDayModel.java
@@ -2,12 +2,23 @@ package com.java.linzexi;
 
 public class EpiDataDayModel {
     int confirmed;
+    int suspected;
     int cured;
     int dead;
 
-    public EpiDataDayModel(int _confirmed, int _cured, int _dead) {
+    public EpiDataDayModel(int _confirmed, int _suspected, int _cured, int _dead) {
         confirmed = _confirmed;
+        suspected = _suspected;
         cured = _cured;
         dead = _dead;
+    }
+    public int getData(int type){
+        if(type == 0)
+            return confirmed;
+        if(type == 1)
+            return suspected;
+        if(type == 2)
+            return cured;
+        return dead;
     }
 }
--- app/src/main/java/com/java/linzexi/EpiDataFragment.java
+++ app/src/main/java/com/java/linzexi/EpiDataFragment.java
@@ -22,12 +22,20 @@ import android.view.ViewGroup;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.google.android.material.tabs.TabLayout;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 public class EpiDataFragment extends Fragment {
     public XRecyclerView dataRecyclerView;
     //    private EpiDataXRecyclerViewAdapter dataRecyclerAdapter;
     private EpiDataViewModel mViewModel;
+
+    private BarChart confirmedBarChart;
+    private BarChart suspectedBatChart;
+    private BarChart curedBarChart;
+    private BarChart deadBarChart;
+    private BarCharts barCharts;
 //    private PopupWindow popupWindow = null;
 //    TextView pop_text = null;
 
@@ -39,12 +47,54 @@ public class EpiDataFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(EpiDataViewModel.class);
+        barCharts = new BarCharts();
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.epi_data_fragment, container, false);
+
+        final TabLayout tabLayout = view.findViewById(R.id.tabLayout_epi_types);
+        tabLayout.addTab(tabLayout.newTab().setText("全球疫情"));
+        tabLayout.addTab(tabLayout.newTab().setText("国内疫情"));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == 0){
+                    barCharts.showBarChart(confirmedBarChart, mViewModel.findData(0, 0, confirmedBarChart), true);
+                    barCharts.showBarChart(suspectedBatChart, mViewModel.findData(0, 1, suspectedBatChart), true);
+                    barCharts.showBarChart(curedBarChart, mViewModel.findData(0, 2, curedBarChart), true);
+                    barCharts.showBarChart(deadBarChart, mViewModel.findData(0, 3, deadBarChart), true);
+                }
+                else{
+                    barCharts.showBarChart(confirmedBarChart, mViewModel.findData(1, 0, confirmedBarChart), true);
+                    barCharts.showBarChart(suspectedBatChart, mViewModel.findData(1, 1, suspectedBatChart), true);
+                    barCharts.showBarChart(curedBarChart, mViewModel.findData(1, 2, curedBarChart), true);
+                    barCharts.showBarChart(deadBarChart, mViewModel.findData(1, 3, deadBarChart), true);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                onTabSelected(tab);
+            }
+        });
+        confirmedBarChart = (BarChart) view.findViewById(R.id.confirmed_chart);
+        suspectedBatChart = (BarChart) view.findViewById(R.id.suspected_chart);
+        curedBarChart = (BarChart) view.findViewById(R.id.cured_chart);
+        deadBarChart = (BarChart) view.findViewById(R.id.dead_chart);
+        barCharts.showBarChart(confirmedBarChart, mViewModel.findData(0, 0, confirmedBarChart), true);
+        barCharts.showBarChart(suspectedBatChart, mViewModel.findData(0, 1, suspectedBatChart), true);
+        barCharts.showBarChart(curedBarChart, mViewModel.findData(0, 2, curedBarChart), true);
+        barCharts.showBarChart(deadBarChart, mViewModel.findData(0, 3, deadBarChart), true);
+
+
 
 //        dataRecyclerView = (XRecyclerView) view.findViewById(R.id.rv_epi_data);
 //        dataRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
